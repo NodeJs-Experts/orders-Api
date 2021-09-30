d5ac0579bc6ff19bb30872a100022c261a7bfe52
aleeixoj <aleeixojunior2016@hotmail.com>
2021-09-30 09:08:17 -0300

    Fix tsconfig

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 
 import 'reflect-metadata';
 import 'express-async-errors';
-import { AppError } from '../../errors/AppErrors';
+import { AppError } from '@errors/AppErrors';
 import '../../containers';
 
 import createConnection from '../typeorm';
