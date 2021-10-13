98b4d4e1f5841058a7c16293b4a52f71f5d5b700
aleeixoj <aleeixojunior2016@hotmail.com>
2021-10-13 11:37:28 -0300

    Fix ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,8 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": true, 
+  "extra": { "ssl": { "rejectUnauthorized": false }},
   "database": `${process.env.PATH_ENV}/shared/infra/typeorm/database.sqlite`,
   "migrations": [`${process.env.PATH_ENV}/shared/infra/typeorm/migrations/*.ts`],
   "entities": [`${process.env.PATH_ENV}/modules/**/entities/*.ts`],
