83d368295ddaead9604773570e6a67700307adbf
aleeixoj <aleeixojunior2016@hotmail.com>
2021-10-13 10:58:57 -0300

    fix babel

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  "database": "./src/shared/infra/typeorm/database.sqlite",
-  "migrations": ["./src/shared/infra/typeorm/migrations/*.ts"],
-  "entities": ["./src/modules/**/entities/*.ts"],
+  "database": `${process.env.PATH_ENV}/shared/infra/typeorm/database.sqlite`,
+  "migrations": [`${process.env.PATH_ENV}/shared/infra/typeorm/migrations/*.ts`],
+  "entities": [`${process.env.PATH_ENV}/modules/**/entities/*.ts`],
   "cli": {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
   }
